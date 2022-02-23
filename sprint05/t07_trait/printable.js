0c3beb08aa2fcac37f5bdfde5c02add2181fea04
Nosov Mykola <nosov2205@gmail.com>
2022-02-23 17:27:51 +0200

    fix: \n

--- sprint05/t07_trait/printable.js
+++ sprint05/t07_trait/printable.js
@@ -4,6 +4,7 @@ module.exports = {
 M134 7.62mm Minigun 
 2 x FN F2000 Tacticals 
 Sidewinder "Ex-Wife" Self-Guided Missile 
-M24 Shotgun Milkor MGL 40mm Grenade Launcher`)
+M24 Shotgun
+Milkor MGL 40mm Grenade Launcher`)
     }
 }
